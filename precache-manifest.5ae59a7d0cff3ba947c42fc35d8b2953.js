90fa8f1c4be6afa82b25d60dc43e0a9978bc9612
mma judith <mmajudith2000@gmail.com>
2020-06-14 12:13:33 -0700

    Updates

---
+++ precache-manifest.5ae59a7d0cff3ba947c42fc35d8b2953.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed58740a57d23147496489eb8364d154",
+    "url": "/shecodeafrica/index.html"
+  },
+  {
+    "revision": "da0148c1488cece03030",
+    "url": "/shecodeafrica/static/css/main.de591de2.chunk.css"
+  },
+  {
+    "revision": "b969fb5b1e218e241361",
+    "url": "/shecodeafrica/static/js/2.f0866ec7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shecodeafrica/static/js/2.f0866ec7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da0148c1488cece03030",
+    "url": "/shecodeafrica/static/js/main.f2798968.chunk.js"
+  },
+  {
+    "revision": "a6dc7d51dff139292c21",
+    "url": "/shecodeafrica/static/js/runtime-main.7aecabeb.js"
+  },
+  {
+    "revision": "6a90f96192b3ec6e4ac7797e70c5defb",
+    "url": "/shecodeafrica/static/media/EstherJolugba.6a90f961.jpg"
+  },
+  {
+    "revision": "60d4c4269960fc64205f5c65db3cbc2d",
+    "url": "/shecodeafrica/static/media/PeaceAmara.60d4c426.jpg"
+  },
+  {
+    "revision": "3a20be062439577bd22d5d0e24fc904a",
+    "url": "/shecodeafrica/static/media/PreciousCadeton.3a20be06.jpg"
+  },
+  {
+    "revision": "3ae13bc863b5aebd90257119aa4b22f0",
+    "url": "/shecodeafrica/static/media/facebook.3ae13bc8.svg"
+  },
+  {
+    "revision": "8e7eeef6b0ca6a973fcaa46c6ab38c47",
+    "url": "/shecodeafrica/static/media/flag.8e7eeef6.svg"
+  },
+  {
+    "revision": "c15b915284e0eb6ace7fd850ec809fe2",
+    "url": "/shecodeafrica/static/media/instagram.c15b9152.svg"
+  },
+  {
+    "revision": "ee93dc00434b67203a9ea468a2d7d4f6",
+    "url": "/shecodeafrica/static/media/she-code-africa-logo.ee93dc00.svg"
+  },
+  {
+    "revision": "45af39539e6327b9a9e6138c5a3c80f2",
+    "url": "/shecodeafrica/static/media/shecodeafrica-landing-img.45af3953.jpg"
+  },
+  {
+    "revision": "1587fa1d50cda71fe61cf7b32df2b5a6",
+    "url": "/shecodeafrica/static/media/target.1587fa1d.svg"
+  },
+  {
+    "revision": "182d5ef4d173a3cd93897242a6fd56a5",
+    "url": "/shecodeafrica/static/media/twitter.182d5ef4.svg"
+  },
+  {
+    "revision": "4bed546b57d1b5e9a9b7ef284ec3d26d",
+    "url": "/shecodeafrica/static/media/value.4bed546b.svg"
+  }
+]);
